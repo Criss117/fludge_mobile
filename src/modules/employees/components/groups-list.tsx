702733b985a697088287d9d3fcea98bff9e69041
Cristian <95005685+Criss117@users.noreply.github.com>
2025-11-03 12:34:23 -0500

    feat: remove groups from employee

---
+++ src/modules/employees/components/groups-list.tsx
@@ -0,0 +1,172 @@
+import { GroupCardWithCheck } from "@/modules/groups/components/group-card-whit-check";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/modules/shared/components/ui/alert-dialog";
+import { Button } from "@/modules/shared/components/ui/button";
+import { Icon } from "@/modules/shared/components/ui/icon";
+import { Text } from "@/modules/shared/components/ui/text";
+import { GroupSummary } from "@/shared/entities/group.entity";
+import * as Haptics from "expo-haptics";
+import { Trash2Icon } from "lucide-react-native";
+import { createContext, use, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { useMutateEmployees } from "../hooks/use.mutate-employees";
+
+interface RootProps {
+  groups: GroupSummary[];
+  children: React.ReactNode;
+}
+
+interface RemoveGroupsAlertProps {
+  businessSlug: string;
+  employeeId: string;
+}
+
+interface Context {
+  selectedGroupsIds: string[];
+  groups: GroupSummary[];
+  onSelectGroup: (groupId: string) => void;
+  clearState: () => void;
+}
+
+const GroupsListContext = createContext<Context | null>(null);
+
+function useGroupsListContext() {
+  const context = use(GroupsListContext);
+
+  if (context === null) {
+    throw new Error("useGroupsListContext must be used within a Provider");
+  }
+
+  return context;
+}
+
+function Root({ children, groups }: RootProps) {
+  const [selectedGroupIds, setSelectedGroupIds] = useState<string[]>([]);
+
+  const onSelectGroup = (groupId: string) => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    if (selectedGroupIds.includes(groupId)) {
+      setSelectedGroupIds(selectedGroupIds.filter((id) => id !== groupId));
+    } else {
+      setSelectedGroupIds([...selectedGroupIds, groupId]);
+    }
+  };
+
+  const clearState = () => {
+    setSelectedGroupIds([]);
+  };
+
+  return (
+    <GroupsListContext.Provider
+      value={{
+        selectedGroupsIds: selectedGroupIds,
+        groups,
+        onSelectGroup,
+        clearState,
+      }}
+    >
+      {children}
+    </GroupsListContext.Provider>
+  );
+}
+
+function List() {
+  const { onSelectGroup, groups, selectedGroupsIds } = useGroupsListContext();
+  return (
+    <>
+      {groups.length === 0 && <Text>No hay grupos asignados</Text>}
+      {groups.map((group) => (
+        <GroupCardWithCheck
+          key={group.id}
+          isSelected={selectedGroupsIds.includes(group.id)}
+          group={group}
+          onPress={() => onSelectGroup(group.id)}
+        />
+      ))}
+    </>
+  );
+}
+
+function RemoveGroupsAlert({
+  businessSlug,
+  employeeId,
+}: RemoveGroupsAlertProps) {
+  const [open, setOpen] = useState(false);
+  const { removeGroups } = useMutateEmployees();
+  const { selectedGroupsIds, clearState } = useGroupsListContext();
+
+  const handleRemovePermissions = () => {
+    if (selectedGroupsIds.length === 0) return;
+
+    removeGroups.mutate(
+      {
+        businessSlug,
+        employeeId,
+        data: {
+          groupIds: selectedGroupsIds,
+        },
+      },
+      {
+        onSuccess: () => {
+          setOpen(false);
+          clearState();
+        },
+      }
+    );
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={(v) => setOpen(v)}>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="rounded-full"
+          variant={selectedGroupsIds.length === 0 ? "outline" : "destructive"}
+          size="icon"
+          disabled={selectedGroupsIds.length === 0}
+        >
+          <Icon as={Trash2Icon} />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Está seguro que desea eliminar estos grupos del empleado?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            El empleado perderá los permisos asignados a estos grupos.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>
+            <Text>Cancelar</Text>
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onPress={handleRemovePermissions}
+            disabled={removeGroups.isPending}
+          >
+            {removeGroups.isPending && (
+              <ActivityIndicator className="text-white" />
+            )}
+            {!removeGroups.isPending && <Text>Continuar</Text>}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export const GroupsList = {
+  List,
+  Root,
+  RemoveGroupsAlert,
+  useGroupsListContext,
+};
--- src/modules/employees/sections/assigned-groups.section.tsx
+++ src/modules/employees/sections/assigned-groups.section.tsx
@@ -1,65 +1,47 @@
-import { GroupCardWithCheck } from "@/modules/groups/components/group-card-whit-check";
-import { Button } from "@/modules/shared/components/ui/button";
 import { Card, CardContent } from "@/modules/shared/components/ui/card";
-import { Icon } from "@/modules/shared/components/ui/icon";
 import { Text } from "@/modules/shared/components/ui/text";
 import type { EmployeeDetail } from "@/shared/entities/employee.entity";
-import * as Haptics from "expo-haptics";
-import { Trash2Icon } from "lucide-react-native";
-import { useState } from "react";
 import { View } from "react-native";
 import { AssignGroupsDialog } from "../components/assign-groups-dialog";
+import { GroupsList } from "../components/groups-list";
 
 interface Props {
   employee: EmployeeDetail;
   businessSlug: string;
 }
 
-export function AssignedGroupsSection({ employee, businessSlug }: Props) {
-  const [selectedGroupIds, setSelectedGroupIds] = useState<string[]>([]);
-
-  const onGroupSelected = (groupId: string) => {
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-    if (selectedGroupIds.includes(groupId)) {
-      setSelectedGroupIds(selectedGroupIds.filter((id) => id !== groupId));
-    } else {
-      setSelectedGroupIds([...selectedGroupIds, groupId]);
-    }
-  };
+function Header({ businessSlug, employee }: Props) {
+  const { selectedGroupsIds } = GroupsList.useGroupsListContext();
 
   return (
-    <View className="flex gap-y-2">
-      <View className="flex flex-row justify-between items-center">
-        <Text variant="h4">
-          Grupos asignados (
-          {selectedGroupIds.length > 0 && `${selectedGroupIds.length}/`}
-          {employee.groups.length})
-        </Text>
-        <View className="flex flex-row gap-x-2">
-          <AssignGroupsDialog businessSlug={businessSlug} employee={employee} />
-          <Button
-            className="rounded-full"
-            variant={selectedGroupIds.length === 0 ? "outline" : "destructive"}
-            size="icon"
-            disabled={selectedGroupIds.length === 0}
-          >
-            <Icon as={Trash2Icon} />
-          </Button>
-        </View>
+    <View className="flex flex-row justify-between items-center">
+      <Text variant="h4">
+        Grupos asignados (
+        {selectedGroupsIds.length > 0 && `${selectedGroupsIds.length}/`}
+        {employee.groups.length})
+      </Text>
+      <View className="flex flex-row gap-x-2">
+        <AssignGroupsDialog businessSlug={businessSlug} employee={employee} />
+        <GroupsList.RemoveGroupsAlert
+          businessSlug={businessSlug}
+          employeeId={employee.id}
+        />
       </View>
-      <Card>
-        <CardContent className="flex gap-y-2">
-          {!employee.groups.length && <Text>No hay grupos asignados</Text>}
-          {employee.groups.map((group) => (
-            <GroupCardWithCheck
-              key={group.id}
-              isSelected={selectedGroupIds.includes(group.id)}
-              group={group}
-              onPress={() => onGroupSelected(group.id)}
-            />
-          ))}
-        </CardContent>
-      </Card>
+    </View>
+  );
+}
+
+export function AssignedGroupsSection({ employee, businessSlug }: Props) {
+  return (
+    <View className="flex gap-y-2">
+      <GroupsList.Root groups={employee.groups}>
+        <Header businessSlug={businessSlug} employee={employee} />
+        <Card>
+          <CardContent className="flex gap-y-2">
+            <GroupsList.List />
+          </CardContent>
+        </Card>
+      </GroupsList.Root>
     </View>
   );
 }
