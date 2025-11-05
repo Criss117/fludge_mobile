c7828c95594c375387537624cee6add9e1dcb5ae
Cristian <95005685+Criss117@users.noreply.github.com>
2025-11-05 00:09:44 -0500

    ref: redisign on register business screen

--- src/modules/businesses/screens/register-business.screen.tsx
+++ src/modules/businesses/screens/register-business.screen.tsx
@@ -5,7 +5,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/modules/shared/components/ui/card";
-import { FieldLabel, FieldSet } from "@/modules/shared/components/ui/field";
 import { Icon } from "@/modules/shared/components/ui/icon";
 import { Building2, CheckCircle } from "lucide-react-native";
 import { View } from "react-native";
@@ -15,47 +14,46 @@ import { CreateBusinessForm } from "../components/create-business-form";
 export function RegisterBusinessScreen() {
   return (
     <KeyboardAwareScrollView>
-      <View className="px-2 flex items-center justify-center flex-1 py-10">
+      <View className="flex gap-y-3 px-4 pt-5">
         <Card className="w-full">
           <CardHeader>
-            <View className="flex flex-row items-center gap-x-2 h-5">
+            <View className="flex flex-row gap-x-2 items-center">
               <Icon as={Building2} size={20} />
-              <CardTitle className="h-5 text-center">
-                Registra tu Negocio
-              </CardTitle>
+              <CardTitle variant="h4">Registra tu Negocio</CardTitle>
             </View>
             <CardDescription>
               Completa la información para comenzar a gestionar tu empresa
             </CardDescription>
           </CardHeader>
-          <CardContent className="flex gap-y-4">
-            <CreateBusinessForm.Root>
-              <FieldSet>
-                <View className="flex items-center flex-row gap-x-2">
-                  <Icon as={CheckCircle} />
-                  <FieldLabel className="font-semibold">
-                    Información básica
-                  </FieldLabel>
-                </View>
-                <CreateBusinessForm.Name />
-                <CreateBusinessForm.Nit />
-                <CreateBusinessForm.LegalName />
-              </FieldSet>
-              <FieldSet>
-                <View className="flex items-center flex-row gap-x-2">
-                  <Icon as={CheckCircle} />
-                  <FieldLabel className="font-semibold">
-                    Informacíon de contacto
-                  </FieldLabel>
-                </View>
-                <CreateBusinessForm.Email />
-                <CreateBusinessForm.Phone />
-                <CreateBusinessForm.Address />
-              </FieldSet>
-              <CreateBusinessForm.Submit />
-            </CreateBusinessForm.Root>
-          </CardContent>
         </Card>
+
+        <CreateBusinessForm.Root>
+          <Card>
+            <CardHeader className="flex items-center flex-row gap-x-2">
+              <Icon as={CheckCircle} />
+              <CardTitle variant="h4">Información básica</CardTitle>
+            </CardHeader>
+            <CardContent className="flex gap-y-2">
+              <CreateBusinessForm.Name />
+              <CreateBusinessForm.Nit />
+              <CreateBusinessForm.LegalName />
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex items-center flex-row gap-x-2">
+              <Icon as={CheckCircle} size={16} />
+              <CardTitle variant="h4">Informacíon de contacto</CardTitle>
+            </CardHeader>
+            <CardContent className="flex gap-y-2">
+              <CreateBusinessForm.Email />
+              <CreateBusinessForm.Phone />
+              <CreateBusinessForm.Address />
+            </CardContent>
+          </Card>
+
+          <CreateBusinessForm.Submit />
+        </CreateBusinessForm.Root>
       </View>
     </KeyboardAwareScrollView>
   );
